69cd522e8af8b1b451f7f3e320560f18183b62ff
saad-aceuss <saadz@aceuss.se>
2025-01-23 09:44:52 +0500

    composition api practice

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,44 @@
-<script>
-export default {
-    data() {
-        return {
-            name: 'Saad',
-            status: true,
-            tasks: [
-                { id: 1, title: 'Task One', completed: false },
-                { id: 2, title: 'Task Two', completed: true },
-                { id: 3, title: 'Task Three', completed: false },
-                { id: 4, title: 'Task Four', completed: true },
-            ],
-            link: 'https://www.google.com',
-        };
-    },
-    methods: {
-        toggleStatus() {
-            this.status = !this.status;
-        }
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const name = ref('John Doe');
+const status = ref(true);
+const newTask = ref('');
+
+const tasks = ref([
+    { id: 1, title: 'Task 1', completed: false },
+    { id: 2, title: 'Task 2', completed: true },
+    { id: 3, title: 'Task 3', completed: false },
+]);
+const link = ref('https://www.google.com');
+const addTask = () => {
+    tasks.value.push({
+        id: tasks.value.length + 1,
+        title: newTask.value,
+        completed: false
+    });
+    newTask.value = '';
+};
+const deleteTask = (id) => {
+    tasks.value = tasks.value.filter(task => task.id !== id);
+};
+
+const toggleStatus = () => {
+    status.value = !status.value;
+};
+
+onMounted((async () => {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await res.json();
+        tasks.value = data; // update tasks
+    } catch (error) {
+        console.log(error);
+
     }
-}
+    console.log('Component mounted');
+}));
+
 </script>
 <template>
     <h1>{{ name }}</h1>
@@ -32,10 +52,21 @@ export default {
     <h3>tasks</h3>
     <ul>
         <li v-for="task in tasks" :key="task.id">
-            {{ task.title }} - {{ task.completed }}
+            <span>
+                {{ task.title }} - {{ task.completed }}
+            </span>
+            <button @click="deleteTask(task.id)">Delete</button>
         </li>
     </ul>
     <br>
     <br>
-    <a v-bind:href="link">Google</a>
+    <!-- <a v-bind:href="link">Google</a> -->
+    <br>
+    <br>
+    <form @submit.prevent="addTask">
+        <label for="newTask"></label>
+        <input type="text" id="newTask" v-model="newTask" name="newTask">
+
+        <button type="submit">Add Task</button>
+    </form>
 </template>
